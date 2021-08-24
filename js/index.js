886f4952fba8a60664b293209124a2d6cc8d53ee
AJavier55 <aliciajavier5@gmail.com>
2021-08-23 21:15:53 -0400

    basic mechanics done

--- js/index.js
+++ js/index.js
@@ -27,4 +27,37 @@ const handOptions = {
       ReferenceError(hand, compHand);
   }
 
+  const referee = (userHand, compHand) => {
+    if (userHand == "paper" && cpHand == "scissors") {
+        setDecision("YOU LOSE!");
+      }
+      if (userHand == "paper" && cpHand == "rock") {
+        setDecision("YOU WIN!");
+        setScore(SCORE + 1);
+      }
+      if (userHand == "paper" && cpHand == "paper") {
+        setDecision("It's a tie!");
+      }
+      if (userHand == "rock" && cpHand == "scissors") {
+        setDecision("YOU WIN!");
+        setScore(SCORE + 1);
+      }
+      if (userHand == "rock" && cpHand == "paper") {
+        setDecision("YOU LOSE!");
+      }
+      if (userHand == "rock" && cpHand == "rock") {
+        setDecision("It's a tie!");
+      }
+      if (userHand == "scissors" && cpHand == "scissors") {
+        setDecision("It's a tie!");
+      }
+      if (userHand == "scissors" && cpHand == "rock") {
+        setDecision("YOU LOSE!");
+      }
+      if (userHand == "scissors" && cpHand == "paper") {
+        setDecision("YOU WIN!");
+        setScore(SCORE + 1);
+      }
+  };
+
   
